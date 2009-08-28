b0cea8765736f31844245453efceecd49e453448
Michael Aye <aye@space.unibe.ch>
2009-08-29 00:21:26 +0200

    new example

--- src/practice_mpl.py
+++ src/practice_mpl.py
@@ -1,22 +1,26 @@
-'''
-Created on Aug 16, 2009
-
-@author: aye
-'''
-import matplotlib.pyplot as plt
+"""
+Show how to modify the coordinate formatter to report the image "z"
+value of the nearest pixel given x and y
+"""
 import numpy as np
-mu, sigma = 100, 15
-x = mu + sigma * np.random.randn(10000)
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
-# the histogram of the data
-n, bins, patches = plt.hist(x, 50, normed=1, facecolor='g', alpha=0.75)
+X = 10*np.random.rand(5,3)
 
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.imshow(X, cmap=cm.jet, interpolation='nearest')
 
-plt.xlabel('Smarts')
-plt.ylabel('Probability')
-plt.title('Histogram of IQ')
-plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
-plt.axis([40, 160, 0, 0.03])
-plt.grid(True)
+numrows, numcols = X.shape
+def format_coord(x, y):
+    col = int(x+0.5)
+    row = int(y+0.5)
+    if col>=0 and col<numcols and row>=0 and row<numrows:
+        z = X[row,col]
+        return 'x=%1.4f, y=%1.4f, z=%1.4f'%(x, y, z)
+    else:
+        return 'x=%1.4f, y=%1.4f'%(x, y)
 
+ax.format_coord = format_coord
 plt.show()
