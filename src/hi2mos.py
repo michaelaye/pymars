7908c46a99b45b136a4b9abd67b68a4f88968fa7
K.-Michael Aye <aye@space.unibe.ch>
2009-10-18 15:28:34 +0200

    implemented option parser and added mapfile option

--- src/hi2mos.py
+++ src/hi2mos.py
@@ -3,34 +3,56 @@
 import ISIS, sys
 from production_notifier import *
 
-args = sys.argv
+from optparse import OptionParser
 
-if len(args) < 2:
-    print "Usage: {0} obsID CCDColour [comma separated list of isis commands, default: ALL]".format(args[0])
-    sys.exit()
-    
-idString = args[1]
-colour = args[2]
+parser = OptionParser()
+usage = "usage: %prog obsID CCDColour [options]"
+descript = "obsID should be in the official form XSP_oooooo_tttt , with XSP = PSP or ESP,"\
+              "oooooo the 6-digit orbit number and tttt the 4-digit target code."\
+              "CCDColour should be one of RED, BG, or IR."
+              
+parser = OptionParser(usage=usage,description=descript)
+parser.add_option("-m", "--mapfile", dest="mapfilename",
+                  help="MAPFILE is the mapping file to be used", metavar="MAPFILE")
+parser.add_option("-d", "--debug",
+                  action="store_true", dest="debug", default=False,
+                  help="print debug messages")
+parser.add_option("-f", "--fake",
+                  action="store_true", dest="fake", default=False,
+                  help="fake the execution, some errors will appear, no real file operation will be done")
+parser.add_option("-c", "--command-list", dest="commandList",
+                  help="per default, the full list of ISIS commands are executed, "
+                        "to create a calibrated map-projected mosaic out of all CCD's "
+                        "that are found for the given CCDColour."
+                        "If you do not want to execute the full list, you can provide here"
+                        "a comma-separated list (no spaces!) of ISIS commands to be run, "
+                        "but they have to be chosen from only the default list (so far),"
+                        "otherwise they are not known in this framework.")
+
+
+(options, args) = parser.parse_args()
+
+print 'args:',args
 
 try:
-    lCommands = (args[3]).split(',')
+    idString = args[0]
+    colour = args[1]
 except:
+    parser.print_help()
+    sys.exit(0)
+
+
+if options.commandList:
+    lCommands = options.commandList.split(',')
+else:
     print "Found no command instruction, Doing all."
     lCommands = None
-
-try:
-    dummy = (args[4])
-    bDebug = True
-except:
-    bDebug = None
-
-try:
-    dummy = (args[5])
-    bFake = True
-except:
-    bFake = None
     
-executer = ISIS.ISIS_Executer(idString,colour, plProgList = lCommands, pbDebug = bDebug, pbFake = bFake)
+executer = ISIS.ISIS_Executer(idString, colour, 
+                              plProgList = lCommands, 
+                              pbDebug = options.debug, 
+                              pbFake = options.fake,
+                              mapfile = options.mapfilename)
 executer.process()
 
 if lCommands == None: lCommands = ['all']
