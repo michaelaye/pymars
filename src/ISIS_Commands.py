b8d11edb8da96c9bfc17d9cf2b5f06d26fcc1be0
K.-Michael Aye <aye@space.unibe.ch>
2009-10-20 11:34:19 +0200

    fixed bug: path contained 'map=' string

--- src/ISIS_Commands.py
+++ src/ISIS_Commands.py
@@ -93,7 +93,7 @@ class ISIS_cam2map(ISIS_cmd):
     sName = 'cam2map'
     def __init__(self):
         ISIS_cmd.__init__(self)
-        self.mapfilePath='map=/usr/local/isis3/data/base/templates/maps/polarstereographic.map'
+        self.mapfilePath='/usr/local/isis3/data/base/templates/maps/polarstereographic.map'
         self.useDefaultMap = True
         self.lParameters.append('pixres=MAP')
         self.bInputToDelete = True
