dd6b3755721d34e1eecb001e551f13b067b88ab5
K.-Michael Aye <aye@space.unibe.ch>
2009-11-10 20:23:52 +0100

    changed old hirise server settings to new hirise server settings

--- src/rsync_parameters.py
+++ src/rsync_parameters.py
@@ -2,8 +2,8 @@ SOURCE = "hisync.lpl.arizona.edu::hirise_data/"
 #  !!remove 'n' here to go hot!!
 PARAM1 = "-avnLh"
 PARAM2 = "--progress"
-DEST_FOLDER_JP2 = "/imago1/RedMosaic_Color-JP2-select/"
-DEST_FOLDER_IMG = "/imago1/EDRgen/"
+DEST_FOLDER_JP2 = "/imgdata/RDRgen/JP2s"
+DEST_FOLDER_IMG = "/imgdata/"
 EXCLUDES = [
   ".*", 
 ]
