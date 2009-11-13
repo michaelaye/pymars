f48835ce94aa5078e6a9e43bc5c78a7991419dcc
K.-Michael Aye <aye@space.unibe.ch>
2009-11-13 17:48:55 +0100

    some cleaning up

--- src/hi2mos.py
+++ src/hi2mos.py
@@ -6,13 +6,16 @@ from optparse import OptionParser
 
 parser = OptionParser()
 usage = "usage: %prog obsID CCDColour mapfile [options]"
-descript = "obsID should be in the official form XSP_oooooo_tttt , with XSP = PSP or ESP,"\
-              "oooooo the 6-digit orbit number and tttt the 4-digit target code."\
-              "CCDColour should be one of RED, BG, or IR."\
-              "mapfile is the path to the mapping file that contains the required information"\
-              "for the mapping, like mapping central coordinate and desired resolution."\
-              "Note that for a mosaic, all elements of the mosaic need to be mapped to the same"\
-              "resolution. See the ISIS website for more info."
+descript = """obsID should be in the official form XSP_oooooo_tttt , with 
+              XSP = PSP or ESP,
+              oooooo the 6-digit orbit number and tttt the 4-digit target code.
+              CCDColour should be one of RED, BG, or IR.
+              mapfile is the path to the mapping file that contains the 
+              required information for the mapping, like mapping central 
+              coordinate and desired resolution.
+              Note that for a mosaic, all elements of the mosaic need to be 
+              mapped to the same resolution. See the ISIS website for more info.
+              """
               
 parser = OptionParser(usage=usage, description=descript)
 parser.add_option("-d", "--debug",
@@ -20,15 +23,22 @@ parser.add_option("-d", "--debug",
                   help="print debug messages")
 parser.add_option("-f", "--fake",
                   action="store_true", dest="fake", default=False,
-                  help="fake the execution, some errors will appear, no real file operation will be done")
-parser.add_option("-c", "--command-list", dest="commandList",
-                  help="per default, the full list of ISIS commands are executed, "
-                        "to create a calibrated map-projected mosaic out of all CCD's "
-                        "that are found for the given CCDColour."
-                        "If you do not want to execute the full list, you can provide here"
-                        "a comma-separated list (no spaces!) of ISIS commands to be run, "
-                        "but they have to be chosen from only the default list (so far),"
-                        "otherwise they are not known in this framework.")
+                  help="""fake the execution, some errors will appear, no real 
+                  file operation will be done""")
+parser.add_option("-c",
+                  "--command-list",
+                  dest="commandList",
+                  help=
+                  """
+                  per default, the full list of ISIS commands are 
+                  executed,
+                  to create a calibrated map-projected mosaic out of all CCD's
+                  that are found for the given CCDColour.
+                  If you do not want to execute the full list, you can provide 
+                  here a comma-separated list (no spaces!) of ISIS commands to
+                  be run, but they have to be chosen from only the default list 
+                  (so far), otherwise they are not known in this framework."""
+                )
 
 
 (options, args) = parser.parse_args()
