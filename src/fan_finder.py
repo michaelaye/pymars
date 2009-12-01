a9e86d880221183b4a6be95d652ed10ab372a6e7
K.-Michael Aye <aye@space.unibe.ch>
2009-12-01 21:41:19 +0100

    binary processing tried out and working. next: plot multiples to compare them.

--- src/fan_finder.py
+++ src/fan_finder.py
@@ -33,8 +33,18 @@ def load_cube_data():
 def grey_processing(inputImg):
     return inputImg
 
-def binary_processing(inputImg):
-    return inputImg
+def binary_processing(inputImg, neighbours):
+    struc8 = np.ones((3, 3))
+    struc4 = [[0, 1, 0], [1, 1, 1], [0, 1, 0]]
+    if neighbours == 4:
+        struc = struc4
+    elif neighbours == 8:
+        struc = struc8
+    else:
+        raise ValueError("numbers of neighbours either 4 or 8")
+        sys.exit(-1)
+    outputImg = nd.morphology.binary_opening(inputImg, struc4)
+    return outputImg
 
 def create_palette():
     palette = plt.cm.gray
@@ -43,61 +53,68 @@ def create_palette():
     palette.set_over('r', 1.0)
     return palette
     
-def labeling():
-    pass
+def labeling(data):
+    struc8 = np.ones((3, 3))
+    labels, n = nd.label(data, struc8)
+    return (labels, n)
 
-def show_masked(myFigure, data, palette):
-    ax = myFigure.add_subplot(111)
+def show_masked(data, palette):
+    myFig = plt.figure(1)
+    ax = myFig.add_subplot(111)
     im = ax.imshow(data, cmap=palette,
                    norm=colors.Normalize(vmin=data.min(),
                                          vmax=data.max(),
                                          clip=True))
     plt.colorbar(im, shrink=0.7)
-    return ax
-    
-def main():
-    arr_big = load_cube_data()
-    arr_big = arr_big[:200, :200]
-    
-    arr_big = grey_processing(arr_big)
-    
-    fig1 = plt.figure(1)
-    ax1 = fig1.add_subplot(111)
-    
-    threshold = find_threshold(arr_big)
-    
-    palette = create_palette()
-    
-    arr_masked = plt.ma.masked_where(arr_big < threshold, arr_big)
-#    im = ax1.imshow(arr_masked, cmap=palette,
-#                    norm=colors.Normalize(vmin=arr_big.min(),
-#                                          vmax=arr_big.max(),
-#                                          clip=True))
-#    
-#    plt.colorbar(im, shrink=0.7)
-    
-    ax1 = show_masked(fig1, arr_masked, palette)
-    
-    arr_bin = plt.where(arr_big < threshold, 1, 0)
-    struc8 = np.ones((3, 3))
-    labels, n = nd.label(arr_bin, struc8)
+    return (myFig, ax)
+  
+def show_binary(data):
+    myFig = plt.figure(1)
+    ax = myFig.add_subplot(111)
+    im = ax.imshow(data)
+    return (myFig, ax)
+
+def annotating(data, labels, n, myAx):
     slices = nd.find_objects(labels)
-        
-    plt.figure(1)
-    plt.title("I/F threshold at {0}, {1} fans found.".format(threshold, n))
     areas = []
     for i in range(n):
         y1 = slices[i][0].start
         y2 = slices[i][0].stop
         x1 = slices[i][1].start
         x2 = slices[i][1].stop
-        area = arr_bin[slices[i]].sum()*0.25
+        area = data[slices[i]].sum()*0.25
         areas.append(area)
         x = (x2 - x1) / 2 + x1
         y = (y2 - y1) / 2 + y1
-        print x, y
-        ax1.annotate(str(i), xy=(x, y), xycoords='data')
-#        ax1.annotate(str(i) + '\n' + str(area) + ' m^2', xy=(x, y), xycoords='data')
+#        print x, y
+#        myAx.annotate(str(i), xy=(x, y), xycoords='data', color='white')
+        myAx.annotate(str(i) + '\n' + str(area) + ' m^2',
+                      xy=(x, y), xycoords='data', color='white')
+    
+def main():
+    orig_img = load_cube_data()
+    small_img = orig_img[:200, :200]
+    
+    preprocced_img = grey_processing(small_img)
+    
+    threshold = find_threshold(preprocced_img)
+    
+    palette = create_palette()
+    
+    arr_masked = plt.ma.masked_where(preprocced_img < threshold, preprocced_img)
+
+#    fig1, ax1 = show_masked(arr_masked, palette)
+    
+    arr_bin = plt.where(preprocced_img < threshold, 1, 0)
+
+    arr_bin1 = binary_processing(arr_bin, 4)
+    labels, n = labeling(arr_bin1)
+    fig1, ax1 = show_binary(arr_bin1)
+    
+    plt.figure(1)
+    plt.title("I/F threshold at {0}, {1} fans found.".format(threshold, n))
+    
+    annotating(arr_bin1, labels, n , ax1)
     
     plt.show()
 
