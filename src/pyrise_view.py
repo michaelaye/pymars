eb234d1f1cc223c8f68fbc8f3cbf218c000c4e0b
aye@space.unibe.ch <aye@space.unibe.ch>
2010-04-01 15:38:25 +0200

    added rectancle selector source as reference

--- src/pyrise_view.py
+++ src/pyrise_view.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python
 
+#from matplotlib.widgets import RectangleSelector
+#from pylab import subplot, arange, plot, sin, cos, pi, show
+#def line_select_callback(event1, event2):
+#    'event1 and event2 are the press and release events'
+#    x1, y1 = event1.xdata, event1.ydata
+#    x2, y2 = event2.xdata, event2.ydata
+#    print "(%3.2f, %3.2f) --> (%3.2f, %3.2f)"%(x1,y1,x2,y2)
+#    print " The button you used were: ",event1.button, event2.button
+#
+#
+#current_ax=subplot(111)                    # make a new plotingrange
+#N=100000                                   # If N is large one can see improvement
+#x=10.0*arange(N)/(N-1)                     # by use blitting!
+#
+#plot(x,sin(.2*pi*x),lw=3,c='b',alpha=.7)   # plot something
+#plot(x,cos(.2*pi*x),lw=3.5,c='r',alpha=.5)
+#plot(x,-sin(.2*pi*x),lw=3.5,c='g',alpha=.3)
+#
+#print "\n      click  -->  release"
+#
+## drawtype is 'box' or 'line' or 'none'
+#LS = RectangleSelector(current_ax, line_select_callback,
+#                       drawtype='box',useblit=True,
+#                       minspanx=5,minspany=5,spancoords='pixels')
+
 from gdal_imports import *
 import tkFileDialog as fd
 import matplotlib.pyplot as plt
