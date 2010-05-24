3a0b2f0e18a09574aaa78940fe41ff8cbb4ece61
aye@space.unibe.ch <aye@space.unibe.ch>
2010-05-24 15:23:13 +0200

    added rebin and rebin_factor

--- src/hirise_tools.py
+++ src/hirise_tools.py
@@ -109,3 +109,24 @@ def executeIsisCmdWithReturn(psArgs):
     else:
         return output
 
+def rebin(a, newshape):
+        '''Rebin an array to a new shape.
+        '''
+        assert len(a.shape) == len(newshape)
+
+        slices = [ slice(0, old, float(old) / new) for old, new in zip(a.shape, newshape) ]
+        coordinates = np.mgrid[slices]
+        indices = coordinates.astype('i')   #choose the biggest smaller integer index
+        return a[tuple(indices)]
+
+def rebin_factor(a, newshape):
+        '''Rebin an array to a new shape.    
+        newshape must be a factor of a.shape.        
+        '''
+        assert len(a.shape) == len(newshape)
+        assert not np.sometrue(np.mod(a.shape, newshape))
+
+        slices = [ slice(None, None, old / new) for old, new in zip(a.shape, newshape) ]
+        return a[slices]
+
+
