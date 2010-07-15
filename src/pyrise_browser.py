42ddf7e629366a451e133ba9824427a305e3816e
K.-Michael Aye <aye@space.unibe.ch>
2010-07-15 19:23:44 +0200

    improved working

--- src/pyrise_browser.py
+++ src/pyrise_browser.py
@@ -9,9 +9,35 @@ from gdal_imports import *
 import matplotlib
 matplotlib.use('Tkagg')
 import matplotlib.pyplot as plt
+from matplotlib.pylab import *
 from matplotlib.widgets import Button
 import sys
 import tkFileDialog as fd
+from matplotlib.widgets import RectangleSelector
+from pylab import subplot, arange, plot, sin, cos, pi, show
+
+def line_select_callback(event1, event2):
+    'event1 and event2 are the press and release events'
+    x1, y1 = event1.xdata, event1.ydata
+    x2, y2 = event2.xdata, event2.ydata
+    print "(%3.2f, %3.2f) --> (%3.2f, %3.2f)" % (x1, y1, x2, y2)
+    print " The button you used were: ", event1.button, event2.button
+
+
+#current_ax=subplot(111)                    # make a new plotingrange
+#N=100000                                   # If N is large one can see improvement
+#x=10.0*arange(N)/(N-1)                     # by use blitting!
+#
+#plot(x,sin(.2*pi*x),lw=3,c='b',alpha=.7)   # plot something
+#plot(x,cos(.2*pi*x),lw=3.5,c='r',alpha=.5)
+#plot(x,-sin(.2*pi*x),lw=3.5,c='g',alpha=.3)
+#
+#print "\n      click  -->  release"
+
+# drawtype is 'box' or 'line' or 'none'
+#LS = RectangleSelector(current_ax, line_select_callback,
+#                       drawtype='box',useblit=True,
+#                       minspanx=5,minspany=5,spancoords='pixels')
 
 def rebin(a, newshape):
         '''Rebin an array to a new shape.
@@ -69,7 +95,7 @@ def downsample(band):
 
 class Cube:
     displaySize = 512
-    overlap = 2 # 1 block is 128, keep 2*128 = 256 in FOV when shifting
+    overlap = 1 # 1 block is 128, keep 2*128 = 256 in FOV when shifting
     def __init__(self, fname):
         self.inDs = gdal.Open(fname, GA_ReadOnly)
         # get image size
@@ -77,15 +103,17 @@ class Cube:
         self.cols = self.inDs.RasterXSize
         self.bands = self.inDs.RasterCount
         self.inBand = self.inDs.GetRasterBand(1)
-        self.inBand.SetNoDataValue = np.NaN
         blockSizes = self.inBand.GetBlockSize()
-        self.xBlockSize = blockSizes[0]
-        self.yBlockSize = blockSizes[1]
+        self.xBlockSize, self.yBlockSize = blockSizes
+        if any(array(blockSizes) == 1):
+            self.xBlockSize = self.yBlockSize = self.displaySize / 2
         self.xOffsets = range(0, self.cols, self.xBlockSize)
         self.yOffsets = range(0, self.rows, self.yBlockSize)
-        self.xInd = len(self.xOffsets) / 2
-        self.yInd = len(self.yOffsets) / 2
+        self.xInd = len(self.xOffsets) // 2
+        self.yInd = len(self.yOffsets) // 2
+        print self.xInd
         self.read_data()
+
 
     def read_data(self, rows=None, cols=None):
         if rows == None:
@@ -96,15 +124,20 @@ class Cube:
                                             self.yOffsets[self.yInd],
                                             rows,
                                             cols)
+    
+    def updateFigure(self):
+        self.im.set_data(self.data)
+        self.ax.set_title(' '.join([str(self.xInd * self.displaySize),
+                                    str(self.yInd * self.displaySize)]))
+        plt.draw()
+        
 
     def up(self, event):
         self.yInd -= self.overlap
         self.yInd = max(self.yInd, 0)
         self.read_data()
+        self.updateFigure()
 
-        im.set_data(self.data)
-        ax.set_title(' '.join([str(self.xInd), str(self.yInd)]))
-        plt.draw()
 
     def down(self, event):
         self.yInd += self.overlap
@@ -114,11 +147,8 @@ class Cube:
         else:
             numRows = self.rows - self.yOffsets[self.yInd]
         self.read_data(rows=numRows)
-
-        im.set_data(self.data)
-        ax.set_title(' '.join([str(self.xInd), str(self.yInd)]))
-        plt.draw()
-
+        self.updateFigure()
+        
     def next(self, event):
         self.xInd += self.overlap
         self.xInd = min(self.xInd, len(self.xOffsets) - 1)
@@ -127,34 +157,38 @@ class Cube:
         else:
             numCols = self.cols - self.xOffsets[self.xInd]
         self.read_data(cols=numCols)
-        im.set_data(self.data)
-        print self.data.mean()
-        ax.set_title(' '.join([str(self.xInd), str(self.yInd)]))
-        plt.draw()
+        self.updateFigure()
 
     def prev(self, event):
         self.xInd -= self.overlap
         self.xInd = max(self.xInd, 0)
         self.read_data()
-        im.set_data(self.data)
-        ax.set_title(' '.join([str(self.xInd), str(self.yInd)]))
-        plt.draw()
-
+        self.updateFigure()
 
 def main():
+    
+    opts = [('mosaic cubes', '.mos.cub'),
+            ('all cubes', '.cub'),
+            ('PDS no label', '.img'),
+            ('PDS w/ label', '.lbl')]
+
     if sys.platform == 'darwin':
-    fname = '/Users/aye/Data/hirise/PSP_003092_0985/PSP_003092_0985_RED.cal.norm.map.equ.mos.cub'
+        options = {}
+        options['filetypes'] = opts
+        fname = fd.askopenfilename(initialdir='/Users/aye/Data/mola', **options)
+#        fname = '/Users/aye/Data/hirise/PSP_003092_0985/PSP_003092_0985_RED.cal.norm.map.equ.mos.cub'
     else:
-    options = {}
-    options['filetypes'] = [('mosaic cubes', '.mos.cub'), ('all cubes', '.cub')]
-    fname = fd.askopenfilename(initialdir='/processed_data', **options)
+        options = {}
+        options['filetypes'] = opts
+        fname = fd.askopenfilename(initialdir='/processed_data', **options)
     
     callback = Cube(fname)
     
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    fig.subplots_adjust(bottom=0.2)
-    im = ax.imshow(callback.data)
+    callback.fig = plt.figure()
+    callback.ax = callback.fig.add_subplot(111)
+    callback.fig.subplots_adjust(bottom=0.2)
+    
+    callback.im = callback.ax.imshow(callback.data)
     
     axprev = plt.axes([0.7, 0.05, 0.1, 0.075])
     axnext = plt.axes([0.81, 0.05, 0.1, 0.075])
@@ -168,6 +202,14 @@ def main():
     bup.on_clicked(callback.up)
     bdown = Button(axdown, 'Down')
     bdown.on_clicked(callback.down)
+    
+    fig2 = figure()
+    small = rebin(callback.data, (512, 512))
+    imshow(small)
+
+    LS = RectangleSelector(callback.ax, line_select_callback,
+                   drawtype='box', useblit=True,
+                   minspanx=5, minspany=5, spancoords='pixels')
     
     plt.show()
 
