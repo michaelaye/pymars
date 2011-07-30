6cb93a3cce00ef4f02a9b931311b0174756bd493
Michael Aye <aye@space.unibe.ch>
2011-07-31 01:53:03 +0200

    updated to vectorization, realised that over time does not make much sense, should implement to plot over an orbit angle

--- tryspice.py
+++ tryspice.py
@@ -1,5 +1,3 @@
-#!/Library/Frameworks/EPD64.framework/Versions/Current/bin/python
-# encoding: utf-8
 """
 tryspice.py
 
@@ -8,31 +6,49 @@ Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 """
 
 from matplotlib.pylab import *
+import numpy as np
 from spice import *
 import datetime
 import sys
 
 furnsh('bepic.mk')
-
 radii = bodvrd('MERCURY','RADII',3)
-daterange=['2021 JAN 30 12:00:00.000', '2021 JAN 31 12:00:00.000']
 
-et1 = utc2et(daterange[0])
-et2 = utc2et(daterange[1])
-print et2utc(et2,'ISOC',5)
-times = arange(et1,et2,60)
-print et2utc(times[-1],'ISOC',5)
+def get_et_array_from_dates(dates, interval):
+    et1 = utc2et(dates[0])
+    et2 = utc2et(dates[1])
+    return arange(et1,et2,interval)
 
-distances = []
-for t in times:
-    print et2utc(t,'ISOC',5)
-    try:
-        pos = spkpos('mercury',t,'j2000','none','mpo')
-    except SpiceException:
-        print 'caught error'
-        sys.exit(1)
-    distances.append(vnorm(pos[0]))
-    
-plot(times-et1,array(distances)-radii[1][0])
-plot(times-et1,array(distances)-radii[1][0],'*')
-show()
+def get_spkpos_distance(et):
+	pos = spkpos('mercury',et,'iau_mercury','none','mpo')
+	return vnorm(pos[0])-radii[1][0]
+
+def get_pnt_distance(et):
+	pnt = subpnt('nearpoint:ellipsoid','mercury',et,'iau_mercury','lt+s','mpo')
+	return vnorm(pnt[0])
+
+vec_spkpos_distance = np.vectorize(get_spkpos_distance)	
+vec_pnt_distance = np.vectorize(get_pnt_distance)
+
+if __name__ == '__main__':
+	dateranges = []
+	dateranges.append(['2021 JAN 30 12:00:00.000', '2021 JAN 31 12:00:00.000'])
+	dateranges.append(['2021 MAY 29 12:00:00.000', '2021 MAY 30 12:00:00.000'])
+
+	times = []
+	for dates in dateranges:
+		times.append(get_et_array_from_dates(dates,60)) # 60 -> per minute
+
+	distances = []
+	for t_array in times:
+	    distances.append(vec_spkpos_distance(t_array))
+	    # distances.append(vec_pnt_distance(t_array))
+
+	# gradients = []
+	# for elem in distances:
+	#     gradients.append(gradient(elem)/60.)
+	
+	# times = array(times[0],times[1])
+	# distances = array(distances[0],distances[1])
+	plot(times[0][:-1],distances[0][:-1]/distances[1])
+	show()
