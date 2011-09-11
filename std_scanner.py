4e90c25e77ff6d87c5695ecde195ec19919db35d
K.-Michael Aye <kmichael.aye@gmail.com>
2011-09-10 17:33:25 -0800

    added runtime status feedback

--- std_scanner.py
+++ std_scanner.py
@@ -18,14 +18,15 @@ def main():
     fnames = glob.glob(search_path)
     cmd_base = ['gdalinfo','-stats']
     std_data = {}
-    for fname in fnames[:10]:
+    length = len(fnames)
+    for i,fname in enumerate(fnames):
+        print i/length*100.0
         cmd = cmd_base+[fname]
         output = sub.Popen(cmd, stdout=sub.PIPE).communicate()[0]
         std = output.split()[-1].split('=')[-1]
         std_data[os.path.basename(fname).split('.cal.des.cub')[0]]=std
     with open('std_data.pkl','w') as f:
         pickle.dump(std_data,f)
-    print std_data
 if __name__ == '__main__':
     main()
 
