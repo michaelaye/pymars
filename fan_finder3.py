dd12a5d44dfe3676e59982f83a2765b455152fb9
K.-Michael Aye <kmichael.aye@gmail.com>
2011-12-06 15:49:34 +0100

    removed feedbacks, converted to Float traits added limit and image plot in horizontal plot container, thresholded next to original data. changed Range to Int with spinner RangeEditor self.data is now 2D not flattened, just the call to kmeans needs .flatten()

--- fan_finder3.py
+++ fan_finder3.py
@@ -2,47 +2,73 @@ from __future__ import division
 from osgeo import gdal
 import numpy as np
 import scipy.ndimage as nd
-from traits.api import File, HasTraits, Str, Button, Array, Int,Enum, on_trait_change
-from traitsui.api import View, Item, UItem,HGroup
 import os
 from scipy.cluster import vq
+# traits imports
+from traits.api import File, HasTraits, Str, Button, Array, Int,Enum, \
+        on_trait_change, Instance, DelegatesTo, Float,Range
+from traitsui.api import View, Item, UItem,HGroup, RangeEditor
+from enable.api import ComponentEditor, Component
+# chaco imports
+from chaco.api import ArrayPlotData, Plot, gray, HPlotContainer
+from chaco.tools.image_inspector_tool import ImageInspectorTool, ImageInspectorOverlay
 
 class KMeans(HasTraits):
     fname = File()
-    feedback = Str
-    feedback2 = Str
     next = Button
     data = Array
-    k_iter = Int(20)
+    k_iter = Enum(20,40,60,80,100)
     k = Int(2)
     framesize = Enum(128,256,512,1024)
-    
-    traits_view = View('framesize',
-                       'k',
-                       'k_iter',
-                       'fname',
-                       HGroup(
-                           UItem('feedback',style='custom'),
-                           UItem('feedback2',style='custom'),
+    limit = Float
+    distortion = Float
+    plot = Instance(Component)
+    traits_view = View(HGroup(
+                        'framesize',
+                        Item('k',
+                             editor=RangeEditor(low=2, high=20, mode='spinner')),
+                        'k_iter'
                        ),
+                       'fname', 'limit','distortion',
+                       UItem('plot',editor=ComponentEditor(size=(700,400)),
+                                height=0.7),
                        resizable=True,
                        buttons=['OK'],
                        )        
     
     @on_trait_change( 'k, k_iter, data')
     def _data_changed(self):
-        codebook, distortion = vq.kmeans(self.data, self.k, self.k_iter )
-        self.feedback += str(codebook.min()) + '\n'
-        self.feedback2 += str(distortion) + '\n'
+        codebook, distortion = vq.kmeans(self.data.flatten(), self.k, self.k_iter )
+        self.limit = codebook.min()
+        self.distortion = distortion
+        data = self.data < self.limit
+        try:
+            self.pd.set_data("imagedata", data)
+        except AttributeError:
+            pass
     
     def _framesize_changed(self):
         self._read_data()
      
+    def _plot_default(self):
+        data = self.data < self.limit
+        pd = self.pd = ArrayPlotData(imagedata=data,orig=self.data)
+        plot1 = Plot(pd, default_origin='top left')
+        plot2 = Plot(pd, default_origin='top left')
+        img_plot1 = plot1.img_plot("imagedata",colormap=gray,padding=0)[0]
+        img_plot2 = plot2.img_plot("orig",colormap=gray,padding=0)[0]
+        container = HPlotContainer(plot1,plot2)
+        container.spacing=0
+        plot1.padding_right=0
+        plot2.padding_left=0
+        plot2.y_axis.orientation= 'right'
+        return container
+        
     def _read_data(self):
         ds = gdal.Open(self.fname)
         self.XSize = ds.RasterXSize
         self.YSize = ds.RasterYSize
-        self.data = ds.ReadAsArray(0,0,self.framesize,self.framesize).flatten()
+        self.data = ds.ReadAsArray(0,0,self.framesize,self.framesize)
         
     def _fname_changed(self):
         if not os.path.exists(self.fname):
