0e3e24e6384daa611c9708d42157803fefb6c90a
K.-Michael Aye <kmichael.aye@gmail.com>
2012-04-11 00:14:55 +0200

    improved integration by summing a lot. ends meet the beginnings now

--- use_mspicer.py
+++ use_mspicer.py
@@ -2,7 +2,7 @@ from __future__ import division
 from mars_spice import MarsSpicer
 from spice import vsep
 import numpy as np
-from matplotlib.pyplot import figure, show
+from matplotlib.pyplot import figure, show, grid
 import datetime as dt
 
 def inner_loop(mspice, ls_res, vector,time_res=3600):
@@ -20,28 +20,32 @@ def inner_loop(mspice, ls_res, vector,time_res=3600):
     start_ls = mspice.l_s
     old_ls = 0
     data = []
+    # while summing up within required ls_res and not crossing 360
     while (mspice.l_s < start_ls + ls_res) and (mspice.l_s > old_ls):
         old_ls = mspice.l_s
         diff_angle = vsep(getattr(mspice,vector), mspice.sun_direction)
-        if np.degrees(diff_angle) > 90:
+        if (mspice.illum_angles.dsolar > 90) or (np.degrees(diff_angle) > 90):
             to_append = 0
         else:
             to_append = mspice.solar_constant * time_res * np.cos(diff_angle)
             # to_append = mspice.solar_constant*np.cos(diff_angle)
         data.append(to_append)
         mspice.advance_time_by(time_res)
-    return data
+    # create the best possible value for the integration by summing:
+    energy = (np.sum(data[:-1]) + np.sum(data[1:]))/2.0
+    return energy, (old_ls - start_ls)
 
 def outer_loop(mspice, end_ls, ls_res, vector, time_res = 3600):
     bigtimes = []
     energies = []
     old_ls = 0
-    while mspice.l_s > old_ls:
+    # while l_s not jumping over 360 and l_s smaller than end criterium:
+    while (mspice.l_s > old_ls) and (mspice.l_s < end_ls):
+        print(mspice.l_s)
         old_ls = mspice.l_s
-        ls_start = mspice.l_s
-        bigtimes.append(ls_start)
-        data = inner_loop(mspice, ls_res, vector, time_res)
-        energies.append(np.sum(data)*10.0/ls_res)
+        bigtimes.append(mspice.l_s + ls_res/2 )
+        energy, delta = inner_loop(mspice, ls_res, vector, time_res)
+        energies.append(energy * 10.0/delta)
     return (bigtimes, energies)
     
 
@@ -55,12 +59,23 @@ def main():
     # set up starting time of analysis, l_s = 0
     mspice.utc = '2011-09-13T14:24:33.733548'
 
+    # mspice.time += dt.timedelta(30)
+    # ls1 = mspice.l_s
+    # utc1 = mspice.utc
+    # mspice.time += dt.timedelta(1)
+    # ls2 = mspice.l_s
+    # mspice.utc = utc1
+    
     # stopping l_s value
     end_ls = 360
 
     # l_s resolution
-    ls_res = 5
+    # ls_res = 0.02
+    ls_res = 5.0
 
+    # time resolution
+    time_res = 3600
+    
     # save this time for multiple runs for resetting mspice each time
     start_time = mspice.time
 
@@ -75,7 +90,7 @@ def main():
     mspice.get_tilted_normal(30)
 
     # first time, save the bigtimes array
-    bigtimes, energies_t30 = outer_loop(mspice, end_ls, ls_res, 'tnormal')
+    bigtimes, energies_t30 = outer_loop(mspice, end_ls, ls_res, 'tnormal',time_res=time_res)
     energies.append(energies_t30)
     labels.append('t30')
 
@@ -85,25 +100,30 @@ def main():
     mspice.rotate_tnormal(90)
 
     mspice.time = start_time
-    energies.append(outer_loop(mspice, end_ls, ls_res, 'trnormal')[1])
+    energies.append(outer_loop(mspice, end_ls, ls_res, 'trnormal',time_res=time_res)[1])
     labels.append('t30_a90')
 
     mspice.time = start_time
-    energies.append(outer_loop(mspice, end_ls, ls_res, 'snormal')[1])
+    energies.append(outer_loop(mspice, end_ls, ls_res, 'snormal',time_res=time_res)[1])
     labels.append('flat')
 
 
     mspice.rotate_tnormal(180)
     mspice.time = start_time
-    energies.append(outer_loop(mspice, end_ls, ls_res, 'trnormal')[1])
+    energies.append(outer_loop(mspice, end_ls, ls_res, 'trnormal',time_res=time_res)[1])
     labels.append('t30,a180')
 
     fig = figure()
     ax = fig.add_subplot(111)
+    grid()
     for energy,label in zip(energies,labels):
-        ax.plot(bigtimes,energy, label = label)
+        ax.plot(bigtimes,energy, '-*', label=label)
+    ax.set_xlabel('L_s [deg]')
+    ax.set_ylabel('Insolation per 10 L_s [MJ]')
+    ax.set_title('Insolation of t_ilted and a_spected (rotated) surfaces at 85 N')
     ax.legend(loc='best')
     show()
+    return (energies, labels)
     
 if __name__ == '__main__':
-    main()
+    energies, labels = main()
