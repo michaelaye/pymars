8cf2d7b7e3b2d6d4740bcc58a0da0f4c220d63e8
K.-Michael Aye <kmichael.aye@gmail.com>
2012-04-11 14:51:46 +0200

    added a lot of plotting beauty

--- aspect_vs_ls.py
+++ aspect_vs_ls.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import numpy as np
-from matplotlib.pyplot import figure, show, cm, grid
+from matplotlib.pyplot import figure, show, cm, grid, subplots
+from matplotlib.ticker import MultipleLocator
 from use_mspicer import outer_loop
 from mars_spice import MarsSpicer
     
@@ -28,14 +29,14 @@ energies = []
 # labels for the plotting
 labels = []
 
-aspects = np.arange(0,180,30)
+aspects = np.arange(0,180,5)
 tilts = [5,30]
 img = np.zeros((len(aspects), 36, 2))
 
 for i,tilt in enumerate(tilts):
-    mspice.time = start_time
     mspice.get_tilted_normal(tilt)
     for j,aspect in enumerate(aspects):
+        mspice.time = start_time
         print('aspect = {0}'.format(aspect))
         mspice.rotate_tnormal(aspect+15) # go to middle of aspect block
         bigtimes, energy = outer_loop(mspice, end_ls, ls_res, 'trnormal')
@@ -47,10 +48,27 @@ for i,tilt in enumerate(tilts):
 img[ img < 1 ] = np.nan
 img5 = img[:,:,0]
 img30 = img[:,:,1]
+images = [img5,img30]
 palette = cm.jet
 palette.set_bad('gray')
-grid()
-fig = figure()
-ax = fig.add_subplot(111)
-ax.imshow(img[:,:,0],origin='lower')
+fig, axes = subplots(2,1,sharex=True, sharey=True)
+ext = [0,360,0,180]
+max = np.nanmax(img5)
+xmajorLocator = MultipleLocator(30)
+xminorLocator = MultipleLocator(10)
+ymajorLocator = MultipleLocator(30)
+yminorLocator = MultipleLocator(10)
+for ax,image in zip(axes,images):
+    im = ax.imshow(image, cmap=palette, vmax=max, origin='lower',extent=ext)
+    ax.set_ylabel('Aspect [deg]')
+    ax.xaxis.set_major_locator(xmajorLocator)
+    ax.yaxis.set_major_locator(ymajorLocator)
+    ax.xaxis.set_minor_locator(xminorLocator)
+    ax.yaxis.set_minor_locator(yminorLocator)
+    ax.grid()
+axes[0].annotate(r'5$^o$ slope',xy=(220,90),backgroundcolor='white',fontsize='large')
+axes[1].annotate(r'30$^o$ slope',xy=(220,90),backgroundcolor='white',fontsize='large')
+cb = fig.colorbar(im,ax=ax, orientation='horizontal', use_gridspec=True,fraction=0.1,aspect=40)
+cb.set_label(r'Insolation [MJ / 10 $L_s$]')
+axes[0].set_xlabel('L_s [deg]')
 show()
