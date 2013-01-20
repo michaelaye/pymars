8753e99a7d437c9d4964bc1a575ef2da3892d31d
K.-Michael Aye <aye@ess.ucla.edu>
2013-01-20 10:43:07 +0100

    adding get_sub_sol_azi and get_sub_sc_azimuth. moving get_time up

--- pdstools.py
+++ pdstools.py
@@ -1,6 +1,6 @@
 from pds.core.common import open_pds
 from pds.core.parser import Parser
-
+from math import cos,sin,acos,radians,degrees
 def get_labels(fname):
     parser = Parser()
     return parser.parse(open_pds(fname))
@@ -16,6 +16,11 @@ def print_root_and_groups(labels):
         else:
             print 'Root item:',key, labels[key]
             
+def get_time(labels=None, fname=None):
+    if fname is not None:
+        labels = get_pds_labels(fname)
+    return labels['TIME_PARAMETERS']['START_TIME']
+    
 def get_angle(labels, group, key):
     value = labels[group][key]
     return float(value.split()[0])
@@ -24,11 +29,6 @@ def get_north_azimuth(labels=None, fname=None):
     if fname is not None:
         labels = get_pds_labels(fname)
     return get_angle(labels,'VIEWING_PARAMETERS','NORTH_AZIMUTH')
-    
-def get_time(labels=None, fname=None):
-    if fname is not None:
-        labels = get_pds_labels(fname)
-    return get_angle(labels,'TIME_PARAMETERS','START_TIME')
     
 def get_incidence(labels=None, fname=None):
     if fname is not None:
@@ -44,6 +44,11 @@ def get_phase(labels=None, fname=None):
     if fname is not None:
         labels = get_pds_labels(fname)
     return get_angle(labels,'VIEWING_PARAMETERS','PHASE_ANGLE')
+
+def get_sub_sol_azi(labels=None, fname=None):
+    if fname is not None:
+        labels = get_pds_labels(fname)
+    return get_angle(labels,'VIEWING_PARAMETERS','SUB_SOLAR_AZIMUTH')
     
 def get_mean_lat(labels):
     maxlat = get_angle(labels,'IMAGE_MAP_PROJECTION','MAXIMUM_LATITUDE')
@@ -54,6 +59,14 @@ def get_mean_lon(labels):
     west_lon = get_angle(labels,'IMAGE_MAP_PROJECTION','WESTERNMOST_LONGITUDE')
     east_lon = get_angle(labels,'IMAGE_MAP_PROJECTION','EASTERNMOST_LONGITUDE')
     return (west_lon+east_lon)/2.0
+   
+def get_sub_sc_azimuth(labels):
+    inc = radians(get_incidence(labels))
+    emis = radians(get_emission(labels))
+    g = radians(get_phase(labels))
+    cosa = cos(g)-(cos(inc)*cos(emis))
+    cosa /= (sin(inc)*sin(emis))
+    return degrees(acos(cosa))
     
 if __name__ == '__main__':
     fname = '/Users/maye/data/hirise/inca/ESP_022607_0985_RED.LBL'
