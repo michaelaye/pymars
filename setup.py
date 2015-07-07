e83fa42cdc59efe630009146d06bbfac4ebbba73
K.-Michael Aye <kmichael.aye@gmail.com>
2015-07-07 15:08:06 -0600

    removing ez setup for being obsolete and TestCommand as I don't want it in setup.py anymore.

--- setup.py
+++ setup.py
@@ -1,22 +1,5 @@
-import ez_setup
-ez_setup.use_setuptools()
 import sys
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = ['-v', '-m', 'not luna']
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
-
 
 setup(
     name = "pymars",
